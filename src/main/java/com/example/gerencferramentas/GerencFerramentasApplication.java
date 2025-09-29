e611f243feff9ddd2d80e550a199269f7adb531c
patrickbiso <patrick.bisolato@uel.br>
2025-09-29 20:29:58 -0300

    chore: projeto inicial Spring Boot (pom, main, properties, .gitignore)

---
+++ src/main/java/com/example/gerencferramentas/GerencFerramentasApplication.java
@@ -0,0 +1,13 @@
+package com.example.gerencferramentas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerencFerramentasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GerencFerramentasApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/gerencferramentas/GerencFerramentasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gerencferramentas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerencFerramentasApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
