cbe7c4c7e3f78a457fd3a253eb8950ca501ecf1d
patrickbiso <patrick.bisolato@uel.br>
2025-09-30 20:55:01 -0300

    Cria a interface FerramentaRepository

---
+++ src/main/java/repository/FerramentaRepository.java
@@ -0,0 +1,13 @@
+package com.example.gerencferramentas.repository;
+
+import com.example.gerencferramentas.model.Ferramenta;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
+import java.util.List;
+
+public interface FerramentaRepository extends JpaRepository<Ferramenta, Long> {
+    Optional<Ferramenta> findByCodigo(String codigo);
+
+    List<Ferramenta> findByNomeContainingIgnoreCase(String nome);
+
+}
